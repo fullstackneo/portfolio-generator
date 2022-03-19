ccb675fdb9d3ab0574645e145a3634523c69e955
Neo <fullstackneo@gmail.com>
2022-03-18 18:23:17 -0600

    using filter and map api to add featured and unfeatured projects

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 const generatePage = require('./src/page-template');
-// const pageHTML = generatePage(name, github);
-// fs.writeFile('index.html', pageHTML, (err) => {
-//   if (err) throw err;
-//   console.log('Portfolio complete! Check out index.html to see the output!');
-// });
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -142,5 +137,8 @@ Add a New Project
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+    fs.writeFile('index.html', pageHTML, err => {
+      if (err) throw err;
+    });
   });
